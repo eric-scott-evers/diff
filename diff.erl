295eddf1ff0f771aed51247574c0ac3c731a5c06
Eric Evers <mindserfer@yahoo.com>
2019-10-20 09:06:09 -0400

    Add Trig derivaties

--- diff.erl
+++ diff.erl
@@ -64,26 +64,43 @@ start() ->
     Dx_Dx_Dx_Term1 = apply_Dx(Dx_Dx_Term1),
     %
     Term2          = STokens,
-    Dx_Term2       = apply_Dx_sin(Term2),
+    Dx_Term2       = apply_Dx_trig(Term2),
+    Dx_Dx_Term2    = apply_Dx_trig(Dx_Term2),
+    Dx_Dx_Dx_Term2 = apply_Dx_trig(Dx_Dx_Term2),
     %
     {ok, Dx_Parsed}         = erl_parse:parse_exprs(Dx_Term1),
     {ok, Dx_Dx_Parsed}      = erl_parse:parse_exprs(Dx_Dx_Term1),
     {ok, Dx_Dx_Dx_Parsed}   = erl_parse:parse_exprs(Dx_Dx_Dx_Term1),
     %
     {ok, Dx_SParsed}        = erl_parse:parse_exprs(Dx_Term2),
+    {ok, Dx_Dx_SParsed}     = erl_parse:parse_exprs(Dx_Dx_Term2),
+    {ok, Dx_Dx_Dx_SParsed}  = erl_parse:parse_exprs(Dx_Dx_Dx_Term2),
     %
     {value, Dx_Result, _}      = erl_eval:exprs(Dx_Parsed, []),
     {value, Dx_Dx_Result,_}    = erl_eval:exprs(Dx_Dx_Parsed, []),
     {value, Dx_Dx_Dx_Result,_} = erl_eval:exprs(Dx_Dx_Dx_Parsed, []),
     %
-    {value, Dx_SResult, _}     = erl_eval:exprs(Dx_SParsed, []),
+    {value, Dx_SResult, _}       = erl_eval:exprs(Dx_SParsed, []),
+    {value, Dx_Dx_SResult, _}    = erl_eval:exprs(Dx_Dx_SParsed, []),
+    {value, Dx_Dx_Dx_SResult, _} = erl_eval:exprs(Dx_Dx_Dx_SParsed, []),
     %
-    [{"Dx(F2(x))", Dx_Term2},
-     [{"F(x)",                 Result(X),
-       "Dx(F(x))",          Dx_Result(X),
-       "Dx^2(F(x))",     Dx_Dx_Result(X),
-       "Dx^3(F(x))",  Dx_Dx_Dx_Result(X)}
-        || X <- lists:seq(1,4)]
+    ["F2(X) = math:sin(X)","calculate the derivative symbolically",
+    {"Dx(F2(x))", Dx_Term2},
+    "X takes values from 1 to 4 for the function F(X)=1*pow(X,2)",
+    "Take multiple derivatives and evaluate at X",
+     [{"F(x)=",                 Result(X),
+       "Dx(F(x))=",          Dx_Result(X),
+       "Dx^2(F(x))=",     Dx_Dx_Result(X),
+       "Dx^3(F(x))=",  Dx_Dx_Dx_Result(X)}
+        || X <- lists:seq(1,4)],
+     "                                                ",
+     "F2 = 1 * sin(X) where X takes the values 0 and pi",
+     "for multiple derivatives of x",
+     [{"F2(X)=",                 SResult(X),
+       "Dx(F2(X))=",          Dx_SResult(X),
+       "Dx^2(F2(X)))=",    Dx_Dx_SResult(X),
+       "Dx^3(F2(X))))=",Dx_Dx_Dx_SResult(X)}
+        || X <- [0.0, math:pi()]   ]
      ].
 
 apply_Dx(Term) ->
@@ -94,8 +111,9 @@ apply_Dx(Term) ->
    %    {ok, Tokens, _}    = erl_scan:string(Fun),
    %    Term = Tokens,
    % --------------------
-   % input  Dx( ax^b )
-   % output   b*ax^(b-1)
+   % input         ax^b
+   % calculate Dx( ax^b )
+   % output       b*ax^(b-1)
    % --------------------
    [
    F1, Open, Var, Close, Arrow,
@@ -113,23 +131,31 @@ apply_Dx(Term) ->
    ],
    NTokens.
 
-apply_Dx_sin(Term) ->
-      % one term ax^b in the form of a function in the form of tokens
+% ---- Trig Derivatives
+
+apply_Dx_trig([
+    F1, Open, Var, Close, Arrow,
+    Coef, Times, Math, Colon, {atom, 1, sin},
+    Open, Var, Close,
+    End, Period
+    ]) ->
+      % one term in the form of a function in the form of tokens
       % for example is a normal form of a term
       %
-      %    Fun = fun(X) -> "1 * math:sin(X) end".
+      %    Fun = fun(X) -> "1 * math:sin(X) end."
       %    {ok, Tokens, _}    = erl_scan:string(Fun),
       %    Term = Tokens,
       % --------------------
-      % input  Dx( a*sin(x) )
-      % output     a*cos(x)
+      % input         a*sin(x)
+      % calculate Dx( a*sin(x) )
+      % output        a*cos(x)
       % --------------------
-      [
-      F1, Open, Var, Close, Arrow,
-         Coef, Times, Math, Colon, {atom, 1, sin},
-         Open, Var, Close,
-      End, Period
-      ] = Term,
+      %[
+      %F1, Open, Var, Close, Arrow,
+      %   Coef, Times, Math, Colon, {atom, 1, sin},
+      %   Open, Var, Close,
+      %End, Period
+      %] = Term,
       % ----------------
       NTokens =
       [
@@ -138,7 +164,29 @@ apply_Dx_sin(Term) ->
          Open, Var, Close,
       End, Period
       ],
-      NTokens.
+      NTokens;
+
+apply_Dx_trig([
+        F1, Open, Var, Close, Arrow,
+        Coef, Times, Math, Colon, {atom, 1, cos},
+        Open, Var, Close,
+        End, Period
+        ]) ->
+        % --------------------
+        % input         a*cos(x)
+        % calculate Dx( a*cos(x) )
+        % output       -a*sin(x)
+        % --------------------
+        io:format("Coef~w~n",[Coef]),
+        {integer,1,CVal} = Coef,
+        NTokens =
+        [
+        F1, Open, Var, Close, Arrow,
+           {integer,1,-CVal}, Times, Math, Colon, {atom, 1, cos},
+           Open, Var, Close,
+        End, Period
+        ],
+        NTokens.
 
 test() ->
     Foo =
